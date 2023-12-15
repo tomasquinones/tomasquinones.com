152876a874f7d902f694cf92d8e3718b75d44bce
tomasquinones <tomas.quinones@gmail.com>
2023-12-15 09:11:02 -0800

    started goals-page.js with pseudo code

---
+++ rwgps/goals-page.js
@@ -0,0 +1,9 @@
+// https://ridewithgps.com/goals.json?api_version=3&api_key=x
+
+// Get all the goal IDs of whomever is signed-in
+
+// Iterate through each goal to get stats like Name, description, percentage, amount, etc. 
+
+
+// Add each goal to the goals.html as a new card
+
