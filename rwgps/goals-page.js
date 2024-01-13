c18c4402585094253224428abe4f4d52ae2bda14
tomasquinones <tomas.quinones@gmail.com>
2024-01-13 09:36:26 -0800

    file clean up, getting rid of experiments and unneeded iterations

--- rwgps/goals-page.js
+++
@@ -1,9 +0,0 @@
-// https://ridewithgps.com/goals.json?api_version=3&api_key=x
-
-// Get all the goal IDs of whomever is signed-in
-
-// Iterate through each goal to get stats like Name, description, percentage, amount, etc. 
-
-
-// Add each goal to the goals.html as a new card
-
